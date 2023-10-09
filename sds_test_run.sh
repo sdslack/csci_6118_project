db94497ce2e53fbaf2ede6909de5e00148994880
Sarah D. Slack <sarah.slack@cuanschutz.edu>
2023-10-09 08:25:19 -0600

    Add bash script for test running

---
+++ sds_test_run.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+set -e  # stop on error
+set -u  # raise error if variable is unset
+set -o pipefail  # fail if any prior step failed
+
+# Script runs print_fires.py given file name, column to query
+# (int), value to query, and optional result column (int).
+
+# Set parameters
+file_name="test/data/LANL_HIV1_2023_seq_metadata.txt"
+categ_column=15
+
+# Run current code
+python src/sds/query_categ_plot.py \
+    --file-name "$file_name" --categ-column $categ_column
