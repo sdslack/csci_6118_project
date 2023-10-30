dd9656b9acb4b4076518c2118b609386e75897ac
Sarah D. Slack <sarah.slack@cuanschutz.edu>
2023-10-30 09:03:25 -0600

    Update for new org

--- src/sds/sds_test_run.sh
+++ src/sds/sds_test_run.sh
@@ -8,10 +8,10 @@ set -o pipefail  # fail if any prior step failed
 # (int), value to query, and optional result column (int).
 
 # Set parameters
-file_name="test/data/LANL_HIV1_2023_seq_metadata.csv"
+file_name="../../test/data/LANL_HIV1_2023_seq_metadata.csv"
 categ_column=24  # subtype
 
 # Run current code
-python src/sds/query_categ_plot.py \
+python query_categ_plot.py \
     --file-name "$file_name" --categ-column $categ_column \
-    --plot-path "test/output" \
+    --plot-path "../../docs" \
