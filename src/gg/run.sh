a7fcd5bd2eb7f9563f0cfcfad2dd1f1cdc006608
ggionet1 <gagi0247@colorado.edu>
2023-10-31 20:29:28 +0000

    Updating functions to make better use of pandas

--- src/gg/run.sh
+++ src/gg/run.sh
@@ -5,7 +5,6 @@ set -u # raise error if variable is unset
 set -o pipefail # fail if any prior step failed
 
 python3 src/gg/query_functions.py
-python3 src/gg/make_query.py --filename -"src/gg/LANL_HIV1_2023_seq_metadata.csv" --query_column -"country" --query_comparison "equal to" --query_value -"Iran"
-python3 src/gg/make_query.py --filename -"src/gg/LANL_HIV1_2023_seq_metadata.csv" --query_column -"sampling_year" --query_comparison "equal to" --query_value -"2023"
-python3 src/gg/data_subsetting.py --filename -"src/gg/LANL_HIV1_2023_seq_metadata.csv"
+python3 src/gg/make_query.py --file_name "src/gg/LANL_HIV1_2023_seq_metadata.csv" --query_column "country" --query_comparison "equal to" --query_value "Iran"
+python3 src/gg/data_subsetting.py --file_name "src/gg/LANL_HIV1_2023_seq_metadata.csv"
 Rscript src/gg/consort_plot.r
