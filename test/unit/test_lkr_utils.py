ec7b16dcff3482dff4c915ad2644f47a563593c3
Laurel Robbins <laurel.robbins@colorado.edu>
2023-11-17 19:10:42 +0000

    added lkr unit tests

---
+++ test/unit/test_lkr_utils.py
@@ -0,0 +1,42 @@
+import unittest
+import sys
+import random
+import os
+import csv
+sys.path.insert(0, '../../src/lkr')  # noqa
+from lkr_utils import count_sequences_by_column
+from lkr_utils import plot_histogram_from_csv
+
+
+class TestDaysFromInfectionPlot(unittest.TestCase):
+
+    def test_basic_functionality(self):
+        csv_file = '../../test/data/input/Fake_HIV.csv'
+        output_file = '../../test/data/output/dfi_output.csv'
+        
+        count_sequences_by_column(csv_file, 0, output_file)
+
+        self.assertTrue(os.path.isfile(output_file))
+
+        expected_count = 6
+        with open(output_file, 'r', newline='') as output:
+            reader = csv.reader(output)
+            next(reader)  # Skip the header
+            for row in reader:
+                days_from_infection, count = map(int, row)
+                if days_from_infection == 30:
+                    self.assertEqual(count, expected_count)
+                    break
+            else:
+                self.fail("Expected count not found in the output file.")
+                
+    def test_plot_creation(self):
+        csv_file = '../../test/data/output/dfi_output.csv'
+        output_png = '../../docs/lkr_dfi_histogram.png'
+
+        plot_histogram_from_csv(csv_file, output_png)
+
+        self.assertTrue(os.path.isfile(output_png))
+
+if __name__ == '__main__':
+    unittest.main()
