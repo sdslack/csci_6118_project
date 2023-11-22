fc0dafdcedc329fdf8fd62ff80759ea185a39b3b
juba2320 <juba2320@colorado.edu>
2023-11-22 04:24:03 +0000

    modularized and added not equal

---
+++ src/jb/get_queried_data.py
@@ -0,0 +1,59 @@
+import argparse
+import query_data as query
+
+parser = argparse.ArgumentParser(
+    description="Query and display data from a CSV file with filters.",
+    prog="Project")
+parser.add_argument("--file",
+                    type=str,
+                    help="Path to the CSV data file",
+                    required=True)
+parser.add_argument("--categorical_filters",
+                    type=str,
+                    help="Filter criteria for categorical variables",
+                    default="")
+parser.add_argument("--numerical_filters",
+                    type=str,
+                    help="Filter criteria for numerical variables",
+                    default="")
+parser.add_argument("--output_file",
+                    type=str,
+                    help="Path to output CSV file",
+                    required=True)
+
+args = parser.parse_args()
+
+def main():
+
+    data = query.load_data(args.file)
+
+    if data is not None:
+        # dictionary for filters
+        filters = {}
+
+        # if there are any categorical filters
+        if args.categorical_filters != "":
+            cat_args = query.split_arguments(args.categorical_filters)
+
+            categorical_filters = query.get_categorical_filters(cat_args)
+            filters.update(categorical_filters)
+
+        # if there are any numerical filters
+        if args.numerical_filters != "":
+            num_args = query.split_arguments(args.numerical_filters)
+
+            numerical_filters = query.get_numerical_filters(num_args)
+            filters.update(numerical_filters)
+
+        # filter the data
+        filtered_data = query.filter_data(data, filters)
+
+        if not filtered_data.empty:
+            filtered_data.to_csv(args.output_file, index=False)
+            print(f"Filtered data saved to {args.output_file}")
+        else:
+            print("No matching data found.")
+
+
+if __name__ == "__main__":
+    main()
--- src/jb/query_data.py
+++ src/jb/query_data.py
@@ -14,6 +14,7 @@ Notes:
 - for numerical filter, filters can be taken as:
     - a range ex. "col:0-7" (exclusive)
     - less than or greater than the field ex. "col<10" or "col>10" (exclusive)
+    - not equal ex. "col!=10"
     - multiple filters ex. "col:7,<5,9-12"
 - for categorical fields, filters are a list of comma separated names
     - ex. "col:banana,red,blue"
@@ -69,7 +70,8 @@ def filter_data(data, filters):
         if data[key].dtype == 'object':
             filtered_data = filtered_data[filtered_data[key].isin(values)]
         # Numerical variable filter
-        else:
+        else: 
+            filtered_data[key] = pd.to_numeric(filtered_data[key], errors = 'coerce')
             for value in values:
                 if '<' in value:
                     filtered_data = filtered_data[
@@ -82,81 +84,75 @@ def filter_data(data, filters):
                     filtered_data = filtered_data[
                         (filtered_data[key] > lower)
                         & (filtered_data[key] < upper)]
+                elif '!=' in value:
+                    filtered_data = filtered_data[filtered_data[key]
+                                                  != float(value[2:])]
                 elif '=' in value:
                     filtered_data = filtered_data[filtered_data[key]
                                                   == float(value[1:])]
     return filtered_data
 
 
-def main():
-    parser = argparse.ArgumentParser(
-        description="Query and display data from a CSV file with filters.",
-        prog="Project")
-    parser.add_argument("--file",
-                        type=str,
-                        help="Path to the CSV data file")
-    parser.add_argument("--categorical_filters",
-                        type=str,
-                        help="Filter criteria for categorical variables",
-                        default="")
-    parser.add_argument("--numerical_filters",
-                        type=str,
-                        help="Filter criteria for numerical variables",
-                        default="")
-    parser.add_argument("--output_file",
-                        type=str,
-                        help="Path to output CSV file",
-                        required=True)
+def split_arguments(filter_parameter):
+    """Split multiple filter arguments by &&.
 
-    args = parser.parse_args()
-
-    data = load_data(args.file)
-
-    if data is not None:
-        # dictionary for filters
-        filters = {}
-
-        # if there are any categorical filters
-        if args.categorical_filters != "":
-            categorical_filters = {}
-            filter_args = args.categorical_filters.strip()
-            cat_args = filter_args.split('&&')
-
-            for arg in cat_args:
-                key, values = arg.split(':')
-                # Remove extra spaces
-                key = key.strip()
-                values = values.split(',')
-                values = [val.strip() for val in values]
-                categorical_filters[key] = values
-            filters.update(categorical_filters)
-
-        # if there are any numerical filters
-        if args.numerical_filters != "":
-            numerical_filters = {}
-            filter_args = args.numerical_filters.strip()
-            num_args = filter_args.split('&&')
-
-            for arg in num_args:
-                key, values = arg.split(':')
-                # Remove extra spaces
-                key = key.strip()
-                values = values.split(',')
-                values = [val.strip() for val in values]  # Remove extra spaces
-                values = [val if any(op in val for op in ['<', '>', '-', '='])
-                          else float(val) for val in values]
-                numerical_filters[key] = values
-            filters.update(numerical_filters)
-
-        # filter the data
-        filtered_data = filter_data(data, filters)
-
-        if not filtered_data.empty:
-            filtered_data.to_csv(args.output_file, index=False)
-            print(f"Filtered data saved to {args.output_file}")
-        else:
-            print("No matching data found.")
+    Parameters
+    ----------
+    filter_parameter: str
+        User input for the filter arguments.
+    Returns
+    -------
+    filter_args
+        List of all filter arguments.
+    """
+    args = filter_parameter.strip()
+    filter_args = args.split('&&')
+    return filter_args
 
 
-if __name__ == "__main__":
-    main()
+def get_numerical_filters(num_args):
+    """Split numerical filters.
+
+    Parameters
+    ----------
+    num_args: list
+        List of columns to be filtered and their filter criteria.
+    Returns
+    -------
+    numerical_filters
+        List of all the numerical filters.
+    """
+    numerical_filters = {}
+    for arg in num_args:
+        key, values = arg.split(':')
+        # Remove extra spaces
+        key = key.strip()
+        values = values.split(',')
+        values = [val.strip() for val in values]
+        values = [val if any(op in val for op in ['<', '>', '-', '='])
+                  else float(val) for val in values]
+        numerical_filters[key] = values
+    return numerical_filters
+
+
+def get_categorical_filters(cat_args):
+    """Split categorical filters.
+
+    Parameters
+    ----------
+    cat_args: list
+        List of columns to be filtered and their filter criteria.
+    Returns
+    -------
+    categorical_filters
+        List of all the categorical filters.
+    """
+    categorical_filters = {}
+    for arg in cat_args:
+        key, values = arg.split(':')
+        # Remove extra spaces
+        key = key.strip()
+        values = values.split(',')
+        values = [val.strip() for val in values]
+        categorical_filters[key] = values
+    return categorical_filters
