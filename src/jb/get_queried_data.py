7ed42c4e1838990a097d95435147a182b2bb7038
juba2320 <juba2320@colorado.edu>
2023-11-22 23:32:03 +0000

    added error handling

--- src/jb/get_queried_data.py
+++ src/jb/get_queried_data.py
@@ -8,13 +8,9 @@ parser.add_argument("--file",
                     type=str,
                     help="Path to the CSV data file",
                     required=True)
-parser.add_argument("--categorical_filters",
+parser.add_argument("--filters",
                     type=str,
-                    help="Filter criteria for categorical variables",
-                    default="")
-parser.add_argument("--numerical_filters",
-                    type=str,
-                    help="Filter criteria for numerical variables",
+                    help="Filter criteria for variables",
                     default="")
 parser.add_argument("--query_output_file",
                     type=str,
@@ -24,10 +20,10 @@ parser.add_argument("--output_columns",
                     type=str,
                     help="Columns to output to queried data file",
                     default="")
-parser.add_argument("--query_request_file",
-                    type=str,
-                    help="Name and path of query request file",
-                    required=True)
+# parser.add_argument("--query_request_file",
+#                     type=str,
+#                     help="Name and path of query request file",
+#                     required=True)
 
 args = parser.parse_args()
 
@@ -35,39 +31,29 @@ def main():
 
     data = query.load_data(args.file)
 
-    if data is not None:
-        # dictionary for filters
-        filters = {}
-
+    if data is not None :
         # if there are any categorical filters
-        if args.categorical_filters != "":
-            cat_args = query.split_arguments(args.categorical_filters)
-
-            categorical_filters = query.get_categorical_filters(cat_args)
-            filters.update(categorical_filters)
-
-        # if there are any numerical filters
-        if args.numerical_filters != "":
-            num_args = query.split_arguments(args.numerical_filters)
-
-            numerical_filters = query.get_numerical_filters(num_args)
-            filters.update(numerical_filters)
+        if args.filters != "":
+            filter_args = query.split_arguments(args.filters)
+            filters = query.get_filters(filter_args)
 
         # filter the data
         output_cols = args.output_columns.split(',')
         filtered_data = query.filter_data(data, filters, output_cols)
 
-        # make the query request summary data frame
-        query_request_df = query.make_query_request_summary(filters, data.columns)
+        # # make the query request summary data frame
+        # query_request_df = query.make_query_request_summary(filters, data.columns)
 
         if not filtered_data.empty:
             filtered_data.to_csv(args.query_output_file, index=False)
-            query_request_df.to_csv(args.query_request_file, index=False)
+            # query_request_df.to_csv(args.query_request_file, index=False)
             print(f"Filtered data saved to {args.query_output_file}")
-            print(f"Query request summary saved to {args.query_request_file}")
+            # print(f"Query request summary saved to {args.query_request_file}")
         else:
             print("No matching data found.")
-
+    
+    else:
+        sys.exit(1)
 
 if __name__ == "__main__":
     main()
--- src/jb/query_data.py
+++ src/jb/query_data.py
@@ -23,10 +23,37 @@ def load_data(file_path):
         return data
     except FileNotFoundError:
         print("File not found. Please provide a valid file path.")
-        sys.exit(1)
+        return None
     except pd.errors.EmptyDataError:
         print("The provided file is empty.")
         return None
+
+
+def check_column_exists(col_names, df):
+    """Make sure the column exists in the data frame.
+
+    Parameters
+    ----------
+    col_names : list
+        Name of the columns to be searched for.
+
+    df: data frame
+        Data frame to search
+    Returns
+    -------
+    existing_columns
+        A list of all existing columns.
+    """
+    existing_columns = []
+
+    for col_name in col_names:
+        try: 
+            df[col_name]
+            existing_columns.append(col_name)
+        except KeyError:
+            print(f"The column '{col_name}' is not present in the DataFrame.")
+
+    return existing_columns
 
 
 def filter_data(data, filters, output_cols):
@@ -49,37 +76,44 @@ def filter_data(data, filters, output_cols):
         Final filtered data frame.
     """
     filtered_data = data
+    # check if column is in data
+    existing_cols = check_column_exists(filters.keys(), data)
+
     for key, values in filters.items():
-        # Categorical variable filter
-        if data[key].dtype == 'object':
-            filtered_data = filtered_data[filtered_data[key].isin(values)]
-        # Numerical variable filter
-        else: 
-            for value in values:
-                if '<' in value:
-                    filtered_data = filtered_data[
-                        filtered_data[key] < float(value[1:])]
-                elif '>' in value:
-                    filtered_data = filtered_data[
-                        filtered_data[key] > float(value[1:])]
-                elif '-' in value:
-                    lower, upper = map(float, value.split('-'))
-                    filtered_data = filtered_data[
-                        (filtered_data[key] > lower)
-                        & (filtered_data[key] < upper)]
-                elif '!=' in value:
-                    filtered_data = filtered_data[filtered_data[key]
-                                                  != float(value[2:])]
-                elif '=' in value:
-                    filtered_data = filtered_data[filtered_data[key]
-                                                  == float(value[1:])]
+        if key in existing_cols:
+            # Categorical variable filter
+            if data[key].dtype == 'object':
+                filtered_data = filtered_data[filtered_data[key].isin(values)]
+            # Numerical variable filter
+            else: 
+                for value in values:
+                    if '<' in value:
+                        filtered_data = filtered_data[
+                            filtered_data[key] < float(value[1:])]
+                    elif '>' in value:
+                        filtered_data = filtered_data[
+                            filtered_data[key] > float(value[1:])]
+                    elif '-' in value:
+                        lower, upper = map(float, value.split('-'))
+                        filtered_data = filtered_data[
+                            (filtered_data[key] > lower)
+                            & (filtered_data[key] < upper)]
+                    elif '!=' in value:
+                        filtered_data = filtered_data[filtered_data[key]
+                                                      != float(value[2:])]
+                    elif '=' in value:
+                        filtered_data = filtered_data[filtered_data[key]
+                                                      == float(value[1:])]
 
     # output specific columns if specified
     if len(output_cols) == 1 and output_cols[0] == '':
         return filtered_data
     else:
+        # check that these column names exist
         col_names = [col.strip() for col in output_cols]
-        return filtered_data[col_names]
+        existing_cols = check_column_exists(col_names, data)
+
+        return filtered_data[existing_cols]
         
 
 
@@ -98,57 +132,35 @@ def split_arguments(filter_parameter):
     """
     args = filter_parameter.strip()
     filter_args = args.split('&&')
-    return filter_args
+
+    # cleans list for any empty values in case extra && is included
+    cleaned_args = [filter.strip() for filter in filter_args if filter]
+    return cleaned_args
 
 
-def get_numerical_filters(num_args):
-    """Split numerical filters.
+def get_filters(filter_args):
+    """Split filters.
 
     Parameters
     ----------
-    num_args: list
+    filter_args: list
         List of columns to be filtered and their filter criteria.
 
     Returns
     -------
-    numerical_filters
-        List of all the numerical filters.
+    filters
+        Dictionary of all filters.
     """
-    numerical_filters = {}
-    for arg in num_args:
+    filters = {}
+    for arg in filter_args:
         key, values = arg.split(':')
         # Remove extra spaces
         key = key.strip()
         values = values.split(',')
-        values = [val.strip() for val in values]
-        values = [val if any(op in val for op in ['<', '>', '-', '='])
-                  else float(val) for val in values]
-        numerical_filters[key] = values
-    return numerical_filters
-
-
-def get_categorical_filters(cat_args):
-    """Split categorical filters.
-
-    Parameters
-    ----------
-    cat_args: list
-        List of columns to be filtered and their filter criteria.
-
-    Returns
-    -------
-    categorical_filters
-        List of all the categorical filters.
-    """
-    categorical_filters = {}
-    for arg in cat_args:
-        key, values = arg.split(':')
-        # Remove extra spaces
-        key = key.strip()
-        values = values.split(',')
-        values = [val.strip() for val in values]
-        categorical_filters[key] = values
-    return categorical_filters
+        # cleans list for any empty values in case extra , is added
+        values = [val.strip() for val in values if val]
+        filters[key] = values
+    return filters
 
 
 def make_query_request_summary(filters, df_columns):
