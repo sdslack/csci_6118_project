eec215931caa79134fdbd5bd939d7e3ed9307483
Sarah D. Slack <sarah.slack@cuanschutz.edu>
2023-11-27 20:43:53 -0700

    Test new plots from LKR output

--- src/sds/sds_test_run.sh
+++ src/sds/sds_test_run.sh
@@ -4,14 +4,27 @@ set -e  # stop on error
 set -u  # raise error if variable is unset
 set -o pipefail  # fail if any prior step failed
 
-# Script runs print_fires.py given file name, column to query
-# (int), value to query, and optional result column (int).
 
+### Testing int counts
 # Set parameters
-file_name="../../test/data/LANL_HIV1_2023_seq_metadata.csv"
-categ_column=24  # subtype
+file_name="../../test/data/output/int_count_output.csv"
 
-# Run current code
-python query_categ_plot.py \
-    --file-name "$file_name" --categ-column $categ_column \
-    --plot-path "../../docs" \
+# Run code
+python plot_hist.py \
+    --file-name "$file_name" --plot-path "../../docs"
+
+### Testing float counts
+# Set parameters
+file_name="../../test/data/output/float_count_output.csv"
+
+# Run code
+python plot_hist.py \
+    --file-name "$file_name" --plot-path "../../docs"
+
+### Testing string counts
+# Set parameters
+file_name="../../test/data/output/str_count_output.csv"
+
+# Run code
+python plot_hist.py \
+    --file-name "$file_name" --plot-path "../../docs"
