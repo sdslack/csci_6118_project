7b050ccc32aa1c4449c53657d3973b6309fe613f
juba2320 <juba2320@colorado.edu>
2023-11-28 06:39:07 +0000

    fixed filtering functionalities

--- src/jb/get_queried_data.py
+++ src/jb/get_queried_data.py
@@ -27,11 +27,12 @@ parser.add_argument("--query_request_file",
 
 args = parser.parse_args()
 
+
 def main():
 
     data = query.load_data(args.file)
 
-    if data is not None :
+    if data is not None:
         # if there are any categorical filters
         if args.filters != "":
             filter_args = query.split_arguments(args.filters)
@@ -42,7 +43,8 @@ def main():
         filtered_data = query.filter_data(data, filters, output_cols)
 
         # make the query request summary data frame
-        query_request_df = query.make_query_request_summary(filters, data.columns)
+        query_request_df = query.make_query_request_summary(filters,
+                                                            data.columns)
 
         if not filtered_data.empty:
             filtered_data.to_csv(args.query_output_file, index=False)
@@ -51,9 +53,10 @@ def main():
             print(f"Query request summary saved to {args.query_request_file}")
         else:
             print("No matching data found.")
-    
+
     else:
         sys.exit(1)
+
 
 if __name__ == "__main__":
     main()
--- test/unit/test_jb_query_utils.py
+++ test/unit/test_jb_query_utils.py
@@ -2,131 +2,197 @@ import unittest
 import random
 import sys
 import pandas as pd
+from pandas.testing import assert_frame_equal
 sys.path.insert(0, '../../src/jb')
-import query_data
+import query_utils as query_data
+
 
 class TestQueryData(unittest.TestCase):
-    
+
     def test_load_data(self):
-        #invalid file 
+        # invalid file
         with self.assertRaises(FileNotFoundError):
             query_data.load_data('data.csv')
 
-        #empty file 
-        self.assertEqual(query_data.load_data('../data/input/empty.csv'), None)
-        
-        #normal file
-        self.assertNotEqual(query_data.load_data('../data/input/Fake_HIV.csv'), None)
-    
+        # empty file
+        with self.assertRaises(pd.errors.EmptyDataError):
+            query_data.load_data('../data/input/empty.csv')
+
+        # normal file
+        file_name = '../data/input/Fake_HIV.csv'
+        self.assertIsInstance(query_data.load_data(file_name),
+                              pd.DataFrame)
+
     def test_check_column_exists(self):
         df = pd.read_csv('../data/input/Fake_HIV.csv')
-        
+
         # nonexistent column
         col_names = ['Sequences', 'Floats']
         with self.assertRaises(KeyError):
-            query_data.check_column_exists(col_names)
-        self.assertEqual(query_data.check_column_exists(col_names, df), [])
-        
+            query_data.check_column_exists(col_names, df)
+
         # existent column
         col_names = ['Sequence', 'Days from Infection']
-        self.assertEqual(query_data.check_column_exists(col_names, df), ['Sequence','Days from Infection'])
-        
+        self.assertEqual(query_data.check_column_exists(col_names, df),
+                         ['Sequence', 'Days from Infection'])
+
         # empty list of columns
         col_names = []
-        self.assertEqual(query_data.check_column_exists(col_names, df), [])
-        
+        self.assertEqual(query_data.check_column_exists(col_names, df),
+                         [])
+
     def test_filter_data(self):
         df = pd.read_csv('../data/input/Fake_HIV.csv')
-        
-        # simple categorical filter
-        filters = {'Sequence':['a','b']}
+
+        # simple = categorical filter
+        filters = {'Sequence': ['=a', '=b']}
         output_cols = ['Sequence']
-        data = {'Sequence': ['a','b','a','b','a','b', 
-                             'a', 'b','a','b','a','b', 
-                             'a','b','a','b','a','b']}
+        data = {'Sequence': ['a', 'b', 'a', 'b', 'a', 'b',
+                             'a', 'b', 'a', 'b', 'a', 'b',
+                             'a', 'b', 'a', 'b', 'a', 'b']}
         filtered_df = pd.DataFrame(data)
-        self.assertEqual(query_data.filter_data(df, filters, output_cols), filtered_df)
-        
+        actual_df = query_data.filter_data(df, filters, output_cols)
+        assert_frame_equal(filtered_df,
+                           actual_df.reset_index(drop=True))
+
         # simple = filter
-        filters = {'Float':['=0.54','=0.88']}
-        output_cols = []
+        filters = {'Float': ['=0.54', '=0.88']}
+        output_cols = ['']
         data = {'Days from Infection': [30, 25],
-                'Sequence': ['c','a'],
+                'Sequence': ['c', 'a'],
                 'Float': [0.88, 0.54]}
         filtered_df = pd.DataFrame(data)
-        self.assertEqual(query_data.filter_data(df, filters, output_cols), filtered_df)
+        actual_df = query_data.filter_data(df, filters, output_cols)
+        assert_frame_equal(filtered_df,
+                           actual_df.reset_index(drop=True))
 
         # simple > and < numerical filter
-        filters = {'Days from Infection':['<21','>29']}
+        filters = {'Days from Infection': ['<=21', '>=29']}
         output_cols = ['Days from Infection']
-        data = {'Days from Infection': [29, 21, 21, 30, 30, 29, 20, 
-                                        20, 29, 30, 21, 30, 30, 20,30]}
+        data = {'Days from Infection': [29, 21, 21, 30, 30, 29, 20,
+                                        20, 29, 30, 21, 30, 30, 20, 30]}
         filtered_df = pd.DataFrame(data)
-        self.assertEqual(query_data.filter_data(df, filters, output_cols), filtered_df)
-        
+        actual_df = query_data.filter_data(df, filters, output_cols)
+        assert_frame_equal(filtered_df,
+                           actual_df.reset_index(drop=True))
+
         # simple - numerical filter
         filters = {'Float': ['0.28-0.41']}
         output_cols = ['Float']
-        data = {'Float': [0.28, 0.39, 0.40, 0.41, 0.28]}
+        data = {'Float': [0.32, 0.28, 0.39, 0.40, 0.41, 0.28]}
         filtered_df = pd.DataFrame(data)
-        self.assertEqual(query_data.filter_data(df, filters, output_cols), filtered_df)
+        actual_df = query_data.filter_data(df, filters, output_cols)
+        assert_frame_equal(filtered_df,
+                           actual_df.reset_index(drop=True))
 
-        # simple != filter
-        filters = {'Sequence': ['!= a', '!= b']}
+        # simple != filter ******************
+        filters = {'Sequence': ['!=a', '!=b']}
         output_cols = ['Sequence']
-        data = {'Sequence': ['c','c','c','c','c','c','c',
-                             'c','c','c','c','c','c','c','c','c']}
+        data = {'Sequence': ['c', 'c', 'c', 'c', 'c', 'c', 'c',
+                             'c', 'c', 'c', 'c', 'c', 'c', 'c', 'c', 'c']}
         filtered_df = pd.DataFrame(data)
-        self.assertEqual(query_data.filter_data(df, filters, output_cols), filtered_df)
+        actual_df = query_data.filter_data(df, filters, output_cols)
+        assert_frame_equal(filtered_df,
+                           actual_df.reset_index(drop=True))
 
         # complex filter
-        filters = {'Sequence': ['= b'], 'Float': ['>0.60'], 'Days from Infection': ['24-30']}
-        output_cols = []
-        data = {'Days from Infection': [27], 
-               'Sequence': ['b'],
-               'Float': [0.61]}
+        filters = {'Sequence': ['=b'], 'Float': ['>=0.60'],
+                   'Days from Infection': ['24-30']}
+        output_cols = ['']
+        data = {'Days from Infection': [27],
+                'Sequence': ['b'],
+                'Float': [0.61]}
         filtered_df = pd.DataFrame(data)
-        self.assertEqual(query_data.filter_data(df, filters, output_cols), filtered_df)
+        actual_df = query_data.filter_data(df, filters, output_cols)
+        assert_frame_equal(filtered_df,
+                           actual_df.reset_index(drop=True))
+
+        # filters = {'Sequence': ['=b', '!=a'],
+        # 'Float': ['>=0.60', '<=0.20'],
+        # 'Days from Infection': ['24-30', '!=']}
+        # output_cols = ['']
+        # data = {'Days from Infection': [27],
+        #        'Sequence': ['b'],
+        #        'Float': [0.61]}
+        # filtered_df = pd.DataFrame(data)
+        # actual_df = query_data.filter_data(df, filters, output_cols)
+        # assert_frame_equal(filtered_df,
+        #                    actual_df.reset_index(drop=True))
 
         # no matching data to filter
-        filters = {'Sequence': ['= q']}
-        output_cols = []
-        data = {}
-        filtered_df = pd.DataFrame(data)
-        self.assertEqual(query_data.filter_data(df, filters, output_cols), filtered_df)
+        filters = {'Sequence': ['=q']}
+        output_cols = ['']
+        column_names = ['Days from Infection', 'Sequence', 'Float']
+        column_types = {'Days from Infection': int,
+                        'Sequence': object,
+                        'Float': float}
+        filtered_df = pd.concat([pd.Series(dtype=column_types[col],
+                                           name=col) for col in column_names],
+                                axis=1)
+        self.assertEqual(filtered_df.empty,
+                         query_data.filter_data(df,
+                                                filters,
+                                                output_cols).empty)
 
     def test_split_arguments(self):
         # with &&
         filter = ' One: 1,1,1 && Two: 2,2 '
-        self.assertEqual(query_data.split_arguments(filter), ['One: 1,1,1 ', ' Two: 2,2'])
-        
+        self.assertEqual(query_data.split_arguments(filter),
+                         ['One: 1,1,1', 'Two: 2,2'])
+
         # with extra delimiter
         filter = 'One: 1,1,1 && Two: 2,2 &&'
-        self.assertEqual(query_data.split_arguments(filter), ['One: 1,1,1 ', ' Two: 2,2 '])
-        
+        self.assertEqual(query_data.split_arguments(filter),
+                         ['One: 1,1,1', 'Two: 2,2'])
+
         # without &&
         filter = 'One: 1,1'
-        self.assertEqual(query_data.split_arguments(filter), ['One: 1,1'])
-        
-        # empty string input 
+        self.assertEqual(query_data.split_arguments(filter),
+                         ['One: 1,1'])
+
+        # empty string input
         filter = ''
         self.assertEqual(query_data.split_arguments(filter), [])
-        
+
     def test_get_filters(self):
-        # simple 
+        # simple
         filter_args = ['Seq : a,b']
-        self.assertEqual(query_data.get_filters(filter_args), {'Seq':['a','b']})
-        
+        self.assertEqual(query_data.get_filters(filter_args),
+                         {'Seq': ['a', 'b']})
+
         # complex
         filter_args = ['Seq: a,b, <10,  !=9,        1-5']
-        self.assertEqual(query_data.get_filters(filter_args), {'Seq':['a','b','<10','!=9', '1-5']})
+        self.assertEqual(query_data.get_filters(filter_args),
+                         {'Seq': ['a', 'b', '<10', '!=9', '1-5']})
         filter_args = ['Seq: a,b, <10, !=9, 1-5 && Floats : =10.0,']
-        cleaned_filter_args = query_data.split_arguments(filter_args)
-        self.assertEqual(query_data.get_filters(cleaned_filter_args), {'Seq':['a','b','<10','!=9', '1-5'], 'Floats': ['=10.0']})
-        
+        cleaned_filter_args = query_data.split_arguments(filter_args[0])
+        self.assertEqual(query_data.get_filters(cleaned_filter_args),
+                         {'Seq': ['a', 'b', '<10', '!=9', '1-5'],
+                          'Floats': ['=10.0']})
+
         # empty filter input
         filter_args = []
         self.assertEqual(query_data.get_filters(filter_args), {})
 
     def make_query_request_summary(self):
-        
+        df = pd.read_csv('../data/input/Fake_HIV.csv')
+
+        # simple
+        filters = {'Sequence': ['=a']}
+        df_cols = df.columns
+        data = {'Search_Options': ['Days from Infection', 'Sequence', 'Float'],
+                'Filter Criteria': ['', '=a', ''],
+                'Do you plan to search by this column?': ['', 'yes', '']}
+        query_request = pd.DataFrame(data)
+        actul_df = query_data.make_query_request_summary(filters, df_cols)
+        self.assertEqual(actual_df, query_request)
+
+        # complex
+        filters = {'Days from Infection': ['<=22', '!=29'],
+                   'Sequence': ['=a', '=b']}
+        df_cols = df.columns
+        data = {'Search_Options': ['Days from Infection', 'Sequence', 'Float'],
+                'Filter Criteria': ['<=22;!=29', '=a;=b', ''],
+                'Do you plan to search by this column?': ['yes', 'yes', '']}
+        actul_df = query_data.make_query_request_summary(filters, df_cols)
+        self.assertEqual(actual_df, query_request)
