b820147cd08bc8394f7d72b6a08b917ec6179e66
Sarah D. Slack <sarah.slack@cuanschutz.edu>
2023-12-03 21:10:48 -0700

    Fix style

--- src/sds/gbq_utils.py
+++ src/sds/gbq_utils.py
@@ -1,3 +1,9 @@
+"""SDS function for Google BigQuery
+
+    * get_gbq_data - Uses pandas-gbq to get specific columns
+
+"""
+
 from google.oauth2 import service_account
 import pandas as pd
 
@@ -5,13 +11,13 @@ import pandas as pd
 # to gain access to the Google BigQuery dataset. The service
 # account ONLY has access to the BigQuery dataset.
 
+
 def get_gbq_data(filters, output_columns):
     """Function uses pandas-gbq to get BigQuery data.
     """
     # Format column names to retrieve from BigQuery
     filter_chunks = filters.strip().split(';')
     filter_cols = [chunk.strip().split(':')[-2] for chunk in filter_chunks]
-    # output_cols = output_columns.strip().split(',')
     output_cols = [col.strip() for col in output_columns.split(',')]
     cols_set = set(filter_cols + output_cols)
 
@@ -28,17 +34,15 @@ def get_gbq_data(filters, output_columns):
     cols_set_str = ", ".join([f"`{col}`" for col in cols_set])
 
     # Query from Google BigQuery using service account
-    project_id="csci6118"
+    project_id = "csci6118"
     credentials = service_account.Credentials.from_service_account_file(
         '../etc/csci6118-ee6fa23ab1b7.json')
     query = f"""
     SELECT {cols_set_str}
     FROM csci6118.lanl_hiv_seq_db.results_all
     """
-    data = pd.read_gbq(query,
-                     project_id=project_id,
-                     credentials=credentials)
-    
+    data = pd.read_gbq(query, project_id=project_id, credentials=credentials)
+
     # Switch column names back into user-queried format
     revert = [
         ('SE_id_SA', 'SE id(SA)'),
@@ -47,5 +51,4 @@ def get_gbq_data(filters, output_columns):
     ]
     for old_val, new_val in revert:
         data.rename(columns={old_val: new_val}, inplace=True)
-
-    return(data)
+    return data
