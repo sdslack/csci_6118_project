8119b0e4aa5407d53134384709e4a64b9dfabfa3
Sarah D. Slack <sarah.slack@cuanschutz.edu>
2023-12-03 20:43:50 -0700

    Get columns from arguments

--- src/sds/test_gcs.py
+++ src/sds/test_gcs.py
@@ -5,22 +5,27 @@ import pandas as pd
 # to gain access to the Google BigQuery dataset. The service
 # account ONLY has access to the BigQuery dataset.
 
-def get_gbq_data(project_id="csci6118"):
+def get_gbq_data(filters, output_columns):
     """Function uses pandas-gbq to get BigQuery data.
     """
-    # --filters "Risk Factor: =Heterosexual, =Sexual Transmission, unspecified" \
-    # --output_columns "SE id(SA), Days from Infection, Risk Factor" \
+    # Format column names to retrieve from BigQuery
+    filter_chunks = filters.split(';')
+    filter_cols = [chunk.split(':')[-2] for chunk in filter_chunks]
+    output_cols = output_columns.split(',')
+    cols_set = set(filter_cols + output_cols)
 
+    # Convert filter_cols into a comma-separated string
+    cols_set_str = ", ".join([f"`{col}`" for col in cols_set])
+
+    # Query from Google BigQuery using service account
+    project_id="csci6118"
     credentials = service_account.Credentials.from_service_account_file(
         '../../etc/csci6118-ee6fa23ab1b7.json')
-    query = """
-    SELECT 'Days from Infection', Subtype
+    query = f"""
+    SELECT {cols_set_str}
     FROM csci6118.lanl_hiv_seq_db.results_all
     """
     data = pd.read_gbq(query,
                      project_id=project_id,
                      credentials=credentials)
-    print(data)
-
-if __name__ == '__main__':
-    get_gbq_data()
+    return(data)
