ab1d348719f61012567504fd7aa9a3d381359244
Laurel Robbins <laurel.robbins@colorado.edu>
2023-12-09 00:02:15 +0000

    fixing lkr pycodestyle

--- src/lkr/lkr_utils.py
+++ src/lkr/lkr_utils.py
@@ -6,24 +6,34 @@ import pandas as pd
 
 def get_args():
     """
-    Parses command line arguments for extracting data from a CSV file and generating a histogram plot.
+    Purpose of function:
+    Parses command line arguments
 
     Returns:
-    argparse.Namespace: Parsed command-line arguments.
+    argparse.Namespace: Parsed command-line arguments
 
     Arguments:
-    - `--csv_file` (str, required): Name of the CSV file.
-    - `--column_name` (str, required): Name of the column for histogram plot.
-    - `--output_file` (str, required): Name of the output file.
+    - `--csv_file` (str, required): Name of the CSV file
+    - `--column_name` (str, required): Name of the column for histogram plot
+    - `--output_file` (str, required): Name of the output file
 
     """
     parser = argparse.ArgumentParser(
-        description='Pull specified data from a CSV file and generate a histogram plot.',
+        description='count specified data from a CSV file',
         prog='lkr_utils'
     )
-    parser.add_argument('--csv_file', type=str, help='CSV file name', required=False)
-    parser.add_argument('--column_name', type=str, help='Column name for histogram', required=False)
-    parser.add_argument('--output_file', type=str, help='Output file name', required=False)
+    parser.add_argument('--csv_file',
+                        type=str,
+                        help='CSV file name',
+                        required=False)
+    parser.add_argument('--column_name',
+                        type=str,
+                        help='Column name for histogram',
+                        required=False)
+    parser.add_argument('--output_file',
+                        type=str,
+                        help='Output file name',
+                        required=False)
 
     args = parser.parse_args()
     return args
@@ -31,7 +41,9 @@ def get_args():
 
 def count_sequences_by_column(csv_file, column_name, output_file):
     """
-    Count the occurrences of values in a specified column of a CSV file and write the results to another CSV file.
+    Purpose of function:
+    1. Count occurrences of strings, floats, or integers in a specified column
+    2. write the results to another CSV file
 
     Args:
     - csv_file (str): The path to the CSV file.
@@ -39,19 +51,19 @@ def count_sequences_by_column(csv_file, column_name, output_file):
     - output_file (str): The path to the output CSV file.
 
     Output CSV Format:
-    - The output CSV will have two columns: the original 'column_name' and 'Counts'.
+    - The output CSV will have two columns:
+            original 'column_name' and 'Counts'
 
     """
-    # Create a dictionary to store the counts
     counts = {}
 
-    # Read the CSV file
+    # read CSV file
     with open(csv_file, newline='', encoding='utf-8-sig') as file:
         reader = csv.DictReader(file)
-        
+
         for row in reader:
             value = row.get(column_name)
-            
+
             if value is not None:
                 # Treat the value as a string
                 if value not in counts:
@@ -59,11 +71,9 @@ def count_sequences_by_column(csv_file, column_name, output_file):
                 else:
                     counts[value] += 1
 
-    # Write the data to a CSV file
+    # output data to a CSV file
     with open(output_file, 'w', newline='') as output:
         writer = csv.writer(output)
         writer.writerow([column_name, 'Counts'])
         for value, count in counts.items():
             writer.writerow([value, count])
-            
-
